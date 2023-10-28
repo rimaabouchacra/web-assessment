21c9ca33d11d644fd183047035e4846f40dd085b
Reema224 <rima.abouchakra.gms@gmail.com>
2023-10-29 00:15:27 +0200

    added menu translation

--- frontend/src/components/menu/index.js
+++ frontend/src/components/menu/index.js
@@ -1,3 +1,48 @@
+// import React from 'react';
+// import Typography from '@mui/material/Typography';
+// import icon1 from '../../images/icon1.png';
+// import icon2 from '../../images/icon2.png';
+// import icon3 from '../../images/icon3.png';
+// import icon4 from '../../images/icon4.png';
+// import './index.css';
+// import { Link as ScrollLink } from 'react-scroll';
+
+// const MenuBar = () => {
+//   return (
+//     <div className="menu-bar">
+//       <ScrollLink to="definition-section" spy={true} smooth={true} duration={500} offset={-50}>
+//         <div className="tab">
+//           <img src={icon1} alt="arrow" className="icon" />
+//           <Typography variant="body1">Definition</Typography>
+//         </div>
+//       </ScrollLink>
+//       <ScrollLink to="jurisdiction-section" spy={true} smooth={true} duration={500} offset={-50}>
+//         <div className="tab">
+//           <img src={icon2} alt="arrow" className="icon" />
+//           <Typography variant="body1">Jurisdiction</Typography>
+//         </div>
+//       </ScrollLink>
+//       <ScrollLink to="violations-section" spy={true} smooth={true} duration={500} offset={-50}>
+//         <div className="tab">
+//           <img src={icon4} alt="arrow" className="icon" />
+//           <Typography variant="body1">Violations</Typography>
+//         </div>
+//       </ScrollLink>
+//       <ScrollLink to="judgments-section" spy={true} smooth={true} duration={500} offset={-50}>
+//         <div className="tab">
+//           <img src={icon3} alt="arrow" className="icon" />
+//           <Typography variant="body1">Judgments</Typography>
+//         </div>
+//       </ScrollLink>
+//     </div>
+//   );
+// };
+
+// export default MenuBar;
+
+
+
+
 import React from 'react';
 import Typography from '@mui/material/Typography';
 import icon1 from '../../images/icon1.png';
@@ -6,32 +51,35 @@ import icon3 from '../../images/icon3.png';
 import icon4 from '../../images/icon4.png';
 import './index.css';
 import { Link as ScrollLink } from 'react-scroll';
+import { useTranslation } from 'react-i18next'; // Import useTranslation
 
 const MenuBar = () => {
+  const { t } = useTranslation(); // Use the t function for translations
+
   return (
     <div className="menu-bar">
       <ScrollLink to="definition-section" spy={true} smooth={true} duration={500} offset={-50}>
         <div className="tab">
           <img src={icon1} alt="arrow" className="icon" />
-          <Typography variant="body1">Definition</Typography>
+          <Typography variant="body1">{t('menu.definition')}</Typography> 
         </div>
       </ScrollLink>
       <ScrollLink to="jurisdiction-section" spy={true} smooth={true} duration={500} offset={-50}>
         <div className="tab">
           <img src={icon2} alt="arrow" className="icon" />
-          <Typography variant="body1">Jurisdiction</Typography>
+          <Typography variant="body1">{t('menu.jurisdiction')}</Typography> 
         </div>
       </ScrollLink>
       <ScrollLink to="violations-section" spy={true} smooth={true} duration={500} offset={-50}>
         <div className="tab">
           <img src={icon4} alt="arrow" className="icon" />
-          <Typography variant="body1">Violations</Typography>
+          <Typography variant="body1">{t('menu.violations')}</Typography> 
         </div>
       </ScrollLink>
       <ScrollLink to="judgments-section" spy={true} smooth={true} duration={500} offset={-50}>
         <div className="tab">
           <img src={icon3} alt="arrow" className="icon" />
-          <Typography variant="body1">Judgments</Typography>
+          <Typography variant="body1">{t('menu.judgments')}</Typography> 
         </div>
       </ScrollLink>
     </div>
@@ -39,9 +87,6 @@ const MenuBar = () => {
 };
 
 export default MenuBar;
-
-
-
 
 
 
