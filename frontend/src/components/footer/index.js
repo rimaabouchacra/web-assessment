f037f3418cbafb3cb2a46e5bbfde331565750acb
Reema224 <rima.abouchakra.gms@gmail.com>
2023-10-29 01:22:05 +0200

    added footer translation

--- frontend/src/components/footer/index.js
+++ frontend/src/components/footer/index.js
@@ -1,13 +1,31 @@
+// import React from 'react';
+// import Typography from '@mui/material/Typography';
+// import './index.css'
+
+// const Footer = () => {
+  
+//   return (
+//     <footer className='footer'>
+//       <Typography variant="body2">
+//         &copy; {new Date().getFullYear()} Criminal Court of Appeal. All Rights Reserved.
+//       </Typography>
+//     </footer>
+//   );
+// };
+
+// export default Footer;
 import React from 'react';
 import Typography from '@mui/material/Typography';
-import './index.css'
+import './index.css';
+import { useTranslation } from 'react-i18next'; // Import useTranslation
 
 const Footer = () => {
-  
+  const { t } = useTranslation(); // Use the t function for translations
+
   return (
     <footer className='footer'>
       <Typography variant="body2">
-        &copy; {new Date().getFullYear()} Criminal Court of Appeal. All Rights Reserved.
+        {t('footer.copyright', { year: new Date().getFullYear() })}
       </Typography>
     </footer>
   );
