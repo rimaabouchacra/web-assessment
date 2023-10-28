4bc917562d0aefb0eb15cb84a7e4275781718ae5
Reema224 <rima.abouchakra.gms@gmail.com>
2023-10-28 15:37:48 +0300

    added violation section

--- frontend/src/components/violations/index.js
+++ frontend/src/components/violations/index.js
@@ -7,15 +7,57 @@ import './index.css';
 const Violation = () => {
   return (
 
-    <div className='container'>
-      <Paper elevation={3} className='boxx'>
-        <div className='circle'>1</div>
-        <Box>
-          <Typography variant="body2" component="div" className='textt'>
-            Your text goes here.
-          </Typography>
-        </Box>
-      </Paper>
+    <div id='violation-section'>
+        <div className='title'>
+            <Typography variant="h3">
+              Violations
+            </Typography><br />
+        </div><br />
+        <div className='container'>
+          <div className='boxx'>
+             <div className='circle'>1</div>
+             <Box>
+               <Typography variant="body2" component="div" className='textt'>
+                Imprisonment, preventive detention or a fine of more than five hundred thousand Lebanese pounds.
+               </Typography>
+             </Box>
+          </div>
+        </div>
+
+
+        <div className='container'>
+          <div className='boxx'>
+             <div className='circle'>2</div>
+             <Box>
+               <Typography variant="body2" component="div" className='textt'>
+                Additional or secondary penalty or personalcompensationsof more than five hundred thousand Lebanese pounds.
+               </Typography>
+             </Box>
+          </div>
+        </div>   
+
+        <div className='container'>
+          <div className='boxx'>
+             <div className='circle'>3</div>
+             <Box>
+               <Typography variant="body2" component="div" className='textt'>
+                Revocation of defense stipulated in Article 73 of the Criminal Procedures Code.
+               </Typography>
+             </Box>
+          </div>
+        </div> 
+
+        <div className='container'>
+          <div className='boxx'>
+             <div className='circle'>4</div>
+             <Box>
+               <Typography variant="body2" component="div" className='textt'>
+                Penalty for a violation correlative with a misdemeanor.
+               </Typography>
+             </Box>
+          </div>
+        </div> 
+      
     </div>
   
   );
